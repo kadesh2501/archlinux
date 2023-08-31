f7beb37fc6eeb1f0f598eba440faf08892b7abac
kadesh2501 <shadeslayer45@gmail.com>
2023-08-31 18:43:37 +0800

    add hook and initram update

--- nvidia-gpu.sh
+++ nvidia-gpu.sh
@@ -5,4 +5,5 @@ sudo pacman -Sy nvtop nvidia lib32-nvidia-utils nvidia-utils
 sudo cp mkinitcpio.conf /etc/mkinitcpio.conf
 sudo mkinitcpio -P
 #add a hook for pacman so kernel is always updated on driver updates
+sudo mkdir /etc/pacman.d/hooks
 sudo cp nvidia.hook /etc/pacman.d/hooks/nvidia.hook
