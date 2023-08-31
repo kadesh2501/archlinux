739fbef56e46d363c1add2b04ae92b9d7574a4a7
kadesh2501 <shadeslayer45@gmail.com>
2023-08-31 18:38:53 +0800

    nvidia updates

--- nvidia-gpu.sh
+++ nvidia-gpu.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #install NVIDIA GPU drivers
 sudo pacman -Sy nvtop nvidia lib32-nvidia-utils nvidia-utils
+#update initram
+sudo cp mkinitcpio.conf /etc/mkinitcpio.conf
+sudo mkinitcpio -P
+#add a hook for pacman so kernel is always updated on driver updates
+sudo cp nvidia.hook /etc/pacman.d/hooks/nvidia.hook
