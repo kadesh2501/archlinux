a98a3107e319601e702d0ea153a902b4f39d4024
kadesh2501 <shadeslayer45@gmail.com>
2023-08-31 21:46:59 +0800

    prompt for conflict fixing

--- setup2.sh
+++ setup2.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #install base stuffs
 sudo pacman -S --noconfirm --needed tldr man nano mc rsync amd-ucode timeshift openssh
+#ssh
+cp sshd_config.conf /etc/ssh/sshd_config.conf
+sudo systemctl enable sshd
+sudo systemctl start sshd
 #install paru
 git clone https://aur.archlinux.org/paru-bin
 cd paru-bin
@@ -11,7 +15,7 @@ paru -Syu
 sudo pacman -Syu
 
 #base hyprland
-paru -S --noconfirm --needed hyprland-git hyprpaper waybar-hyprland-git xdg-desktop-portal-wlr wlroots xdg-desktop-portal polkit-kde-agent rofi-lbonn-wayland-git wezterm kitty pcmanfm-qt neovim gedit brightnessctl pavucontrol alsa-utils grim slurp mpv vvave librewolf-bin wlogout network-manager-applet  udiskie
+paru -S --needed hyprland-git hyprpaper waybar-hyprland-git xdg-desktop-portal-wlr wlroots xdg-desktop-portal polkit-kde-agent rofi-lbonn-wayland-git wezterm kitty pcmanfm-qt neovim gedit brightnessctl pavucontrol alsa-utils grim slurp mpv vvave librewolf-bin wlogout network-manager-applet  udiskie
 
 #theme
 paru -S --noconfirm --needed catppuccin-gtk-theme-mocha vimix-cursors  tela-icon-theme 
@@ -21,10 +25,7 @@ git clone https://github.com/vs66388/hyprland.git
 cp -r ~/Downloads/hyprland/.config/ ~/.config/
 #wallpaper to be at ~/.config/hypr/hyprpaper.conf
 
-#ssh
-cp sshd_config.conf /etc/ssh/sshd_config.conf
-sudo systemctl enable sshd
-sudo systemctl start sshd
+
 
 #copy modified configs
 #cp keybinds.conf ~/.config/hypr/keybinds.conf
