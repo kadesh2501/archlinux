6a32ae2416602208fbdfa02667f4511a2c538af5
kadesh2501 <shadeslayer45@gmail.com>
2023-09-06 16:50:38 +0800

    hyperv gpu accerlation doesn't exist

--- hyperv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#install drivers for hyperv
-sudo pacman -S --noconfirm --needed hyperv
-sudo systemctl enable hv_fcopy_daemon.service
-sudo systemctl enable hv_kvp_daemon.service
-sudo systemctl enable hv_vss_daemon.service
-sudo systemctl start hv_fcopy_daemon.service
-sudo systemctl start hv_kvp_daemon.service
-sudo systemctl start hv_vss_daemon.service
