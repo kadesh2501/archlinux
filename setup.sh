7f31c17044799e8d4512e1618d7042e4fedd163c
kadesh2501 <admin@gstechservices.com.au>
2023-10-07 00:12:59 +0800

    setup montior timeout

--- setup.sh
+++ setup.sh
@@ -39,11 +39,13 @@ paru -S --needed --noconfirm sddm-nordic-theme-git
 #install hyprland itself
 paru -S hyprland hyprpaper waybar-hyprland-git xdg-desktop-portal-wlr wlroots xdg-desktop-portal \
 polkit-kde-agent rofi-lbonn-wayland-git wezterm pcmanfm-qt brightnessctl alsa-utils \
-grim slurp wlogout network-manager-applet udiskie thunar dunst xord-xwayland thunar-archive-plugin thunar-media-tags-plugin swayidle
+grim slurp wlogout network-manager-applet udiskie thunar dunst xord-xwayland thunar-archive-plugin thunar-media-tags-plugin swayidle swaylock sway-audio-idle-inhibit-git
 #set some defaults
 xdg-settings set default-web-browser firefox.desktop #make firefox default web handler
 xdg-mime default thunar.desktop inode/directory #make thunar default file handler
 xdg-mime default firefox.desktop x-scheme-handler/https x-scheme-handler/http #make defualt firefox URL handler
+#make support scripts executable
+chmod +x ~/.config/hypr/scripts/swayidle.sh
 #theme
 sudo pacman -S --needed ttf-font-awesome ttc-iosevka noto-fonts-cjk playerctl
 paru -S --noconfirm --needed catppuccin-gtk-theme-mocha vimix-cursors tela-icon-theme otf-font-awesome ttf-jetbrains-mono-nerd
