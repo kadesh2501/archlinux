878df535ad4e87ae32e24ff34fb6ef4bff37105d
kadesh2501 <admin@gstechservices.com.au>
2023-10-11 17:25:42 +0800

    fix typos

--- stablediffusion.sh
+++ stablediffusion.sh
@@ -2,7 +2,7 @@ sudo usermod -aG render $USER
 sudo usermod -aG video $USER
 #will default to putting everything in /home
 cd ~
-paru python3.10 -y
+paru python3.10
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui && cd stable-diffusion-webui
 python3.10 -m venv venv
 ./webui.sh --precision full --no-half --skip-torch-cuda-check
