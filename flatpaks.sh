4c332666836804dd575f3d54848500573d0af642
kadesh2501 <admin@gstechservices.com.au>
2023-12-10 10:06:29 +0800

    comment out some flatpaks

--- flatpaks.sh
+++ flatpaks.sh
@@ -4,7 +4,6 @@ flatpak install --user flathub dev.goats.xivlauncher
 #flatseal
 flatpak install --user flathub com.github.tchx84.Flatseal
 #some other apps
-#flatpak install --user flathub com.discordapp.Discord
 flatpak install --user flathub com.visualstudio.code
 #protonup-qt
 flatpak install --user flathub net.davidotek.pupgui2 
@@ -15,6 +14,6 @@ flatpak install --user flathub com.discordapp.Discord
 #Disk Usage Analyser
 flatpak install --user flathub org.gnome.baobab
 
-flatpak install --user flathub org.audacityteam.Audacity
-flatpak install --user flathub org.blender.Blender
-flatpak install --user flathub fr.handbrake.ghb
+#flatpak install --user flathub org.audacityteam.Audacity
+#flatpak install --user flathub org.blender.Blender
+#flatpak install --user flathub fr.handbrake.ghb
